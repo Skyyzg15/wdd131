741c8f5aa396f9f3cd8444e3f1cc38d7b76a9a25
Skyyzg15 <cielozegarragarcia@gmail.com>
2026-07-23 19:00:04 -0500

    Temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,11 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").innerHTML = document.lastModified; 'Last Modified: ${document.lastModified}';
+
+const navigation = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEvenListener('click', () => {
+    navigation.classList.toggle('close');
+    hambutton.classList.toggle('close');
+})
